7529b3cfd73209ee7ad5197d08ce806a5db5b1ca
Miruna Marin <marinlarisa14@gmail.com>
2025-09-07 19:37:40 +0300

    Add project files.

---
+++ Animal_sounds/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace App1_animal_sounds
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Known animals: Dog, Cat, Cow, Duck");
+            Console.Write("Choose an animal: ");
+            string animal = Console.ReadLine();
+
+            string sound;
+
+            switch (animal)
+            {
+                case "Dog": sound = "woof"; break;
+                case "Cat": sound = "meow"; break;
+                case "Cow": sound = "moo"; break;
+                case "Duck": sound = "quack"; break;
+                default: sound = "(unknown animal)"; break;
+            }
+
+            Console.WriteLine($"{animal} makes sound: {sound}");
+
+            Console.WriteLine("\nPress Enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
